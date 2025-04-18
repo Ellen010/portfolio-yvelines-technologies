3ef45c35b99c353b52f4b4888d27944be3496713
Ellen010 <elenawelty@gmail.com>
2025-04-18 17:07:41 +0200

    navbar design corrections1

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
@@ -6,7 +5,6 @@ import Navbar from "././sections/Navbar.jsx";
 import React from 'react'
 
 function App() {
-    const [count, setCount] = useState(0)
 
     return (
         <>
