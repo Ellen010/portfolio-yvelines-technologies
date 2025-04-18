2d5f8e0dd5a99928b8002baf2bfbc56055f173d6
Ellen010 <elenawelty@gmail.com>
2025-04-18 17:02:23 +0200

    navbar design

--- src/sections/Navbar.jsx
+++ src/sections/Navbar.jsx
@@ -23,16 +23,17 @@ const Navbar = () => {
     return (
         <header className="fixed top-0 left-0 right-0 z-50 bg-black/90">
             <div className="max-w-7xl mx-auto">
-                <div className="flex justify-between items-center py-5 mx-auto c-space">
-                    <a href="/" className="text-neutral-400 font-bold text-xl hover:text-white transition-colors">
-                        Yvelines Technologies
-                    </a>
+                <div className="nav-top">
+                    <img src="/logo.png" alt="logo" className="logo" />
+                    <p  className="title">
+                        New Digital Solutions
+                    </p>
 
                     <button
                         onClick={toggleMenu}
-                        className="text-neutral-400 hover:text-white focus:outline-none sm:hidden flex"
+                        className="btn-menu"
                         aria-label="Toggle menu">
-                        <img src={isOpen ? 'assets/close.svg' : 'assets/menu.svg'} alt="toggle" className="w-6 h-6" />
+                        <img src={isOpen ? '../assets/close.svg' : '../assets/menu.svg'} alt="toggle" className="btn-menu-image" />
                     </button>
 
                     <nav className="sm:flex hidden">
