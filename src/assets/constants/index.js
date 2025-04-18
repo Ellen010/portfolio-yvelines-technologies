6ef5c7a58bbb4a0921548f63b935715c8bdf0875
Ellen010 <elenawelty@gmail.com>
2025-04-18 11:18:06 +0200

    navbar and constants

---
+++ src/assets/constants/index.js
@@ -0,0 +1,286 @@
+export const navLinks = [
+    {
+        id: 1,
+        name: 'Home',
+        href: '#home',
+    },
+    {
+        id: 2,
+        name: 'About',
+        href: '#about',
+    },
+    {
+        id: 3,
+        name: 'Work',
+        href: '#work',
+    },
+    {
+        id: 4,
+        name: 'Contact',
+        href: '#contact',
+    },
+];
+
+export const clientReviews = [
+    {
+        id: 1,
+        name: 'Anna Simon',
+        position: 'Director at Annas Business Consulting',
+        img: 'assets/review1.png',
+        review:
+            'Working with Yvelines Technoligies was a fantastic experience. My old website was transformed into a modern, user-friendly platform. Attention to details and commitment to quality during this project was unmatched. I highly recommend this company for any web dev projects.',
+    },
+    {
+        id: 2,
+        name: 'More info is coming soon',
+        position: 'More info is coming soon',
+        img: 'assets/review2.png',
+        review:
+            'More info is coming soon',
+    },
+    {
+        id: 3,
+        name: 'More info is coming soon',
+        position: 'More info is coming soon',
+        img: 'assets/review2.png',
+        review:
+            'More info is coming soon',
+    },
+    {
+        name: 'More info is coming soon',
+        position: 'More info is coming soon',
+        img: 'assets/review2.png',
+        review:
+            'More info is coming soon', },
+];
+
+export const myProjects = [
+    {
+        title: '3Degrees - 3D Modeling Platform',
+        desc: '3Degrees is a personalised website that represents a 3D models for model inspiration and private orders. Website has a modern and secure frontend user interface using Next.JS and TailwindCSS, delivering a refined and optimized user experience.',
+        subdesc:
+            'Designed custom animations and graphics using Three.js and GSAP, including scroll triggers, staggered effects, timelines, and a progress-tracking video slider.',
+        href: 'https://3degrees.vercel.app/',
+        texture: '/textures/project/project1.mp4',
+        logo: '/assets/project-logo1.png',
+        logoStyle: {
+            backgroundColor: '#2A1816',
+            border: '0.2px solid #36201D',
+            boxShadow: '0px 0px 60px 0px #AA3C304D',
+        },
+        spotlight: '/assets/spotlight1.png',
+        tags: [
+            {
+                id: 1,
+                name: 'React.js',
+                path: '/assets/react.svg',
+            },
+            {
+                id: 2,
+                name: 'TailwindCSS',
+                path: 'assets/tailwindcss.png',
+            },
+            {
+                id: 3,
+                name: 'TypeScript',
+                path: '/assets/typescript.png',
+            },
+            {
+                id: 4,
+                name: 'Framer Motion',
+                path: '/assets/framer.png',
+            },
+        ],
+    },
+    {
+        title: 'More info is coming soon',
+        desc: 'More info is coming soon',
+        subdesc:
+            'More info is coming soon',
+        href: 'More info is coming soon',
+        texture: '/textures/project/project2.mp4',
+        logo: '/assets/project-logo2.png',
+        logoStyle: {
+            backgroundColor: '#13202F',
+            border: '0.2px solid #17293E',
+            boxShadow: '0px 0px 60px 0px #2F6DB54D',
+        },
+        spotlight: '/assets/spotlight2.png',
+        tags: [
+            {
+                id: 1,
+                name: 'React.js',
+                path: '/assets/react.svg',
+            },
+            {
+                id: 2,
+                name: 'TailwindCSS',
+                path: 'assets/tailwindcss.png',
+            },
+            {
+                id: 3,
+                name: 'TypeScript',
+                path: '/assets/typescript.png',
+            },
+            {
+                id: 4,
+                name: 'Framer Motion',
+                path: '/assets/framer.png',
+            },
+        ],
+    },
+    {
+        title: 'More info is coming soon',
+        desc: 'More info is coming soon',
+        subdesc:
+            'More info is coming soon',
+        href: 'More info is coming soon',
+        texture: '/textures/project/project3.mp4',
+        logo: '/assets/project-logo3.png',
+        logoStyle: {
+            backgroundColor: '#60f5a1',
+            background:
+                'linear-gradient(0deg, #60F5A150, #60F5A150), linear-gradient(180deg, rgba(255, 255, 255, 0.9) 0%, rgba(208, 213, 221, 0.8) 100%)',
+            border: '0.2px solid rgba(208, 213, 221, 1)',
+            boxShadow: '0px 0px 60px 0px rgba(35, 131, 96, 0.3)',
+        },
+        spotlight: '/assets/spotlight3.png',
+        tags: [
+            {
+                id: 1,
+                name: 'React.js',
+                path: '/assets/react.svg',
+            },
+            {
+                id: 2,
+                name: 'TailwindCSS',
+                path: 'assets/tailwindcss.png',
+            },
+            {
+                id: 3,
+                name: 'TypeScript',
+                path: '/assets/typescript.png',
+            },
+            {
+                id: 4,
+                name: 'Framer Motion',
+                path: '/assets/framer.png',
+            },
+        ],
+    },
+    {
+        title: 'More info is coming soon',
+        desc: 'More info is coming soon',
+        subdesc:
+            'More info is coming soon',
+        href: 'More info is coming soon',
+        texture: '/textures/project/project4.mp4',
+        logo: '/assets/project-logo4.png',
+        logoStyle: {
+            backgroundColor: '#0E1F38',
+            border: '0.2px solid #0E2D58',
+            boxShadow: '0px 0px 60px 0px #2F67B64D',
+        },
+        spotlight: '/assets/spotlight4.png',
+        tags: [
+            {
+                id: 1,
+                name: 'React.js',
+                path: '/assets/react.svg',
+            },
+            {
+                id: 2,
+                name: 'TailwindCSS',
+                path: 'assets/tailwindcss.png',
+            },
+            {
+                id: 3,
+                name: 'TypeScript',
+                path: '/assets/typescript.png',
+            },
+            {
+                id: 4,
+                name: 'Framer Motion',
+                path: '/assets/framer.png',
+            },
+        ],
+    },
+    {
+        title: 'More info is coming soon',
+        desc: 'More info is coming soon',
+        subdesc:
+            'More info is coming soon',
+        href: 'More info is coming soon',
+        texture: '/textures/project/project5.mp4',
+        logo: '/assets/project-logo5.png',
+        logoStyle: {
+            backgroundColor: '#1C1A43',
+            border: '0.2px solid #252262',
+            boxShadow: '0px 0px 60px 0px #635BFF4D',
+        },
+        spotlight: '/assets/spotlight5.png',
+        tags: [
+            {
+                id: 1,
+                name: 'React.js',
+                path: '/assets/react.svg',
+            },
+            {
+                id: 2,
+                name: 'TailwindCSS',
+                path: 'assets/tailwindcss.png',
+            },
+            {
+                id: 3,
+                name: 'TypeScript',
+                path: '/assets/typescript.png',
+            },
+            {
+                id: 4,
+                name: 'Framer Motion',
+                path: '/assets/framer.png',
+            },
+        ],
+    },
+];
+
+export const calculateSizes = (isSmall, isMobile, isTablet) => {
+    return {
+        deskScale: isSmall ? 0.05 : isMobile ? 0.06 : 0.065,
+        deskPosition: isMobile ? [0.5, -4.5, 0] : [0.25, -5.5, 0],
+        cubePosition: isSmall ? [4, -5, 0] : isMobile ? [5, -5, 0] : isTablet ? [5, -5, 0] : [9, -5.5, 0],
+        reactLogoPosition: isSmall ? [3, 4, 0] : isMobile ? [5, 4, 0] : isTablet ? [5, 4, 0] : [12, 3, 0],
+        ringPosition: isSmall ? [-5, 7, 0] : isMobile ? [-10, 10, 0] : isTablet ? [-12, 10, 0] : [-24, 10, 0],
+        targetPosition: isSmall ? [-5, -10, -10] : isMobile ? [-9, -10, -10] : isTablet ? [-11, -7, -10] : [-13, -13, -10],
+    };
+};
+
+export const workExperiences = [
+    {
+        id: 1,
+        name: '3Degrees Inc',
+        pos: 'Software Developer',
+        duration: '2024 - Present',
+        title: "3Degrees Inc. is an innovative startup inspired to bring the difference in 3D modeling, inform more people about 3D modeling and share new models and concepts.",
+        icon: '/assets/framer.svg',
+        animation: 'victory',
+    },
+    {
+        id: 2,
+        name: 'NoPestsAllowed',
+        pos: 'Software Developer- Full Stack ',
+        duration: '2024',
+        title: "NoPestsAllowed's aim is to provide worldwide application to centralize deposition about pests. Each deposition is verified by picture and user location. The Picture is geolocalised in order to define the deposition location. The picture must be a visual proof showing pests or many depositions against a place.",
+        icon: '/assets/figma.svg',
+        animation: 'clapping',
+    },
+    {
+        id: 3,
+        name: 'Telus',
+        pos: 'Data Analyst',
+        duration: '2023',
+        title: "Telus is an AI company that combines the power of human intelligence and high-quality data to design. It's goals to train and improve your AI models and applications.",
+        icon: '/assets/notion.svg',
+        animation: 'salute',
+    },
+];
