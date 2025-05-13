52e75f56c4ae1cb2c1e24b8a7de3425bada9ac42
Ellen010 <elenawelty@gmail.com>
2025-05-13 17:37:09 +0200

    components corrections

--- src/App.jsx
+++ src/App.jsx
@@ -6,10 +6,10 @@ import Navbar from './sections/Navbar.jsx'
 import Hero from './sections/Hero.jsx'
 import About from './sections/About.jsx'
 import Footer from './sections/Footer.jsx'
-/*import Contact from './sections/Contact.jsx'
+import Contact from './sections/Contact.jsx'
 import Clients from './sections/Clients.jsx'
 import Projects from './sections/Projects.jsx'
-import WorkExperience from './sections/Experience.jsx'*/
+import WorkExperience from './sections/Experience.jsx'
 
 function App() {
 
@@ -19,10 +19,11 @@ function App() {
                 <Navbar />
                 <Hero />
                 <About />
-
+                <Projects />
                 <Clients />
-
-
+                <WorkExperience />
+                <Contact />
+                <Footer />
             </div>
 
             <p className="read-the-docs">
